a02a26af3fff06e9c524490b90280ff7e02d1fbf
Yoshiよし <38394135+yosiyoshi@users.noreply.github.com>
2018-12-18 07:12:49 +0900

    Update main2.py

--- main2.py
+++ main2.py
@@ -4,7 +4,6 @@ Created on Thu Nov 29 18:16:15 2018
 
 @author: YosiYoshi
 """
-import re
 from flask import Flask, render_template, request
 from ynltk import OmnibusStem
 app = Flask(__name__)
@@ -16,12 +15,10 @@ def form():
 @app.route('/result', methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
-      result = request.form
-      for key, value in result.items():
-          txt1 = re.sub(' ', '', key)
-          txt2 = re.sub(' ', '', value)
+      getkey = request.form['key']
+      getvalue = request.form['value']
       s=OmnibusStem()
-      result2 = s.compStemmer(txt1.lower(),txt2.lower())
+      result2 = s.compStemmer(getkey.lower(),getvalue.lower())
       return 'Result: {}'.format(result2)
 
 if __name__ == "__main__":
