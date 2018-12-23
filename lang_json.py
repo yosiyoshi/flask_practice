9c7f7ca50f85777d3cf9ae594dd2916a155fba90
Yoshiよし <38394135+yosiyoshi@users.noreply.github.com>
2018-12-23 18:11:46 +0900

    Add files via upload

---
+++ lang_json.py
@@ -0,0 +1,24 @@
+﻿# e.g.: curl -i -H "Content-Type: application/json" -d "{\"amount\":5000.0,\"description\":\"salary\"}" http://localhost:5000/data
+from flask import Flask, jsonify, request
+from ynltk import Langvowel
+
+app = Flask(__name__)
+l = Langvowel()
+
+data = []
+
+@app.route('/data')
+def get_data():
+  return jsonify(data)
+
+@app.route('/lang')
+def detec_lang():
+  return l.langvowel(str(data))
+
+@app.route('/data', methods=['POST'])
+def add_data():
+  data.append(request.get_json())
+  return '', 204
+
+if __name__ == '__main__':
+    app.run(host='localhost',debug=True)
